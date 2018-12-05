2d449363d5c315fbb8ea9ba68a212f19265eea3a
Komal <komalmehta.8380@gmail.com>
2018-12-05 22:09:23 +0530

    Add Template.

--- mysite/bridge/models.py
+++ mysite/bridge/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class CompanyName(models.Model):
+    company_name = models.TextField(max_length=50)
+
+
+class UserComapny(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    company_id = models.ForeignKey(CompanyName,on_delete=models.CASCADE)
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mysite.bridge.views import index1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/index1',index1,name='index1')
+
 ]
